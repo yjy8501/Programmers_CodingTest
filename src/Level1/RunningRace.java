697670b17be3967f14666b1dfe310f84dc31bcdd
JaeYong <yjy850146@gmail.com>
2023-09-21 15:03:05 +0900

    [Solution] 프로그래머스 1단계 달리기 경주

---
+++ src/Level1/RunningRace.java
@@ -0,0 +1,53 @@
+package Level1;
+
+import java.util.HashMap;
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+        String[] answer = new String[players.length];
+        // 선수들의 이름, 등수를 해쉬 맵에 저장
+        HashMap<String, Integer> nameHash = new HashMap<>();
+        HashMap<Integer, String> rankHash = new HashMap<>();
+
+        for(int i = 0; i < players.length; i++) {
+            nameHash.put(players[i], i);
+            rankHash.put(i, players[i]);
+        }
+
+        for(String player : callings) {
+            // 이름을 통해 등수를 불러온다
+            int rank = nameHash.get(player);
+            // 등수 -1로 앞에 등수의 선수의 이름을 불러오고, 등수를 +1 해준다
+            String frontPlayer = rankHash.get(rank-1);
+            nameHash.put(frontPlayer, nameHash.get(frontPlayer)+1);
+            nameHash.put(player, nameHash.get(player)-1);
+            rankHash.put(rank-1, player);
+            rankHash.put(rank, frontPlayer);
+
+        }
+        for(int i = 0; i < players.length; i++) {
+            answer[i] = rankHash.get(i);
+        }
+
+        return answer;
+    }
+
+}
+
+public class RunningRace {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String[] player = {"mumu", "soe", "poe", "kai", "mine"};
+        String[] calling = {"kai", "kai", "mine", "mine"};
+
+        String[] answer = solution.solution(player, calling);
+
+        for (String s : answer) {
+            System.out.print(s+" ");
+        }
+    }
+}
+
+
