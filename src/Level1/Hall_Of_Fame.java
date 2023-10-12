f0ed63259ac931b01d47596ebaa0091127f9a933
JaeYong <yjy850146@gmail.com>
2023-10-12 17:43:37 +0900

    [Solution] 프로그래머스 1단계 명예의 전당(1)

---
+++ src/Level1/Hall_Of_Fame.java
@@ -0,0 +1,20 @@
+package Level1;
+
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class Hall_Of_Fame {
+    public int[] solution(int k, int[] score) {
+        int[] answer = new int[score.length];
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+
+        for(int i = 0; i < score.length; i++) {
+            priorityQueue.add(score[i]);
+            if(priorityQueue.size() > k)
+                priorityQueue.poll();
+            answer[i] = priorityQueue.stream().min(Integer::compareTo).get();
+        }
+
+        return answer;
+    }
+}
